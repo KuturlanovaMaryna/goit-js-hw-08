f7b5684d154bec895db4e519f65c580ca3e4c7ff
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-08-25 20:37:36 +0300

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,7 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) );
 
 
 player.on('timeupdate', throttle(onPlay, 1000));
